807882a8837867a0c30c9c0ebe6c7650c4fb76f7
vkkasi <vkkasi76@gmail.com>
2022-07-05 11:53:00 +0900

    수정

--- src/views/schneider/widget/IntergratedTemperatureHumidityState/index.js
+++ src/views/schneider/widget/IntergratedTemperatureHumidityState/index.js
@@ -1,7 +1,22 @@
 import { Card, CardHeader, CardTitle, CardBody, Row, Col } from 'reactstrap'
 import { Settings } from 'react-feather'
 
+import axios from 'axios';
+import { useEffect, useState } from 'react';
+
 const IntergratedTemperatureHumidityState = () => {
+  const instance = axios.create({
+    withCredentials: true,
+    headers: {
+      'Access-Control-Allow-Origin': '*',
+      'Content-Type': 'application/json',
+    },
+  })
+
+  // const [weatherData, setWeatherData] = useState([])
+  const [temp, setTemp] = useState('')
+  const [humi, setHumi] = useState('')
+
   const transactionsArr1 = [
     {
       name: '외부',
@@ -89,6 +104,44 @@ const IntergratedTemperatureHumidityState = () => {
     })
   }
 
+
+  const getWeather = () => {
+    let tmp;
+    const date = new Date();	// 현재 날짜 및 시간
+    const year = `${date.getFullYear()}`;	// 년도
+    tmp = date.getMonth() + 1;
+    const month = (tmp < 10) ? `0${tmp}` : `${tmp}`	// 월
+    tmp = date.getDate();
+    const day = (tmp < 10) ? `0${tmp}` : `${tmp}`	// 일
+    tmp = date.getHours();
+    const hours = (tmp < 10) ? `0${tmp}00` : `${tmp}00`	// 시간
+    console.log(year, month, day, hours)
+    
+    fetch(`https://apis.data.go.kr/1360000/VilageFcstInfoService_2.0/getUltraSrtNcst?serviceKey=FfOFAORgfXGq7hk%2FD0i8pglsdu%2FNc9WdGGQzwWGA8uK3n7XZo55cY75JV7sqwpWLcqYics7%2B%2FEqNJOgCLDs%2FFw%3D%3D&pageNo=1&numOfRows=1000&dataType=JSON&base_date=${year}${month}${day}&base_time=${hours}&nx=61&ny=125`)
+    .then(response => response.json())
+    .then(response => {
+      // console.log(response.response.body.items.item)
+      // setWeatherData(response.response.body.items.item)
+      const weatherData = response.response.body.items.item
+
+      weatherData.map(item => {
+        if (item.category === 'T1H') setTemp(item.obsrValue)
+        if (item.category === 'REH') setHumi(item.obsrValue)
+      })
+
+
+
+    })
+    .catch(error => {
+      console.log('error', error);
+    })
+
+  }
+
+  useEffect(() => {
+    getWeather();
+  }, [])
+
   return (
     <Card className='card-transaction'>
       <CardHeader>
@@ -97,6 +150,10 @@ const IntergratedTemperatureHumidityState = () => {
         <Settings size={18} className='cursor-pointer' />
       </CardHeader>
       <CardBody>
+        <div>
+          <p>온도 : {temp}</p>
+          <p>습도 : {humi}</p>
+        </div>
         <Row>
           <Col>{renderTransactions(transactionsArr1)}</Col>
           <Col>{renderTransactions(transactionsArr2)}</Col>
